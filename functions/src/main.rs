a2b7363d2785dec83c288a53c6f5feea35b08515
Rajesh <45181827+gilgamesh7@users.noreply.github.com>
2025-01-23 08:28:02 +0000

    Block expressions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,18 @@
 fn main() {
     another_function(5);
+
+    block_expressions();
 }
 
 fn another_function(x: i32)-> () {
     println!("The value of x is: {x}");
+}
+
+fn block_expressions() {
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {y}");
 }
