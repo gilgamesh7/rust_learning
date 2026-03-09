70f80cd88a7287b75ca508ab89c018d23b3d7a70
gilgamesh7 <felidaesrule@yahoo.com>
2026-03-09 21:45:01 +1300

    Clone - Deep copy

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -6,4 +6,10 @@ fn main() {
     let y = x; // Move ownership of the vector to y
     // println!("{:?}", x); // This will cause a compile-time error because x is no longer valid
     println!("{:?}", y); // This will work because y now owns the vector
+
+    // Clone
+    let x = vec!["Hello".to_string()]; // On the heap
+    let y = x.clone(); // Clone the vector, creating a new one on the heap
+    println!("{:?}", x); // This will work because x is still valid
+    println!("{:?}", y); // This will also work because y is a clone of x
 }
